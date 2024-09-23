6fee0b7c3f635dd860b2beb802071d51b5c77b8f
xyvie lyons <xyvielyons@gmail.com>
2024-09-23 20:28:42 +0300

    first commit

---
+++ controllers/arduino.controller.js
@@ -0,0 +1,24 @@
+import { publishMessage } from "../server.js";
+export const arduinoPostMessage=async(req,res,next)=>{
+    try {
+        publishMessage(req.body.topic,req.body.message)
+        res.status(200).json({
+            status:"message sent to pump"
+        })
+    } catch (error) {
+       console.log(error);
+       res.status(200).json({
+        status:"error",
+        error
+       })
+    }
+}
+
+export const arduinoGetMessage=async(req,res,next)=>{
+    try {
+    
+    } catch (error) {
+        
+    }
+}
+
